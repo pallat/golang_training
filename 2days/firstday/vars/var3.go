559bc3cd7665e0f490d0d0b728d42e9c9205360d
pallat <yod.pallat@gmail.com>
2017-03-09 23:02:51 +0700

    variable declaration

---
+++ 2days/firstday/vars/var3.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s = "world"
+	fmt.Println("Hello,", s)
+}
---
+++ 2days/firstday/vars/var4.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "world"
+	fmt.Println("Hello,", s)
+}
