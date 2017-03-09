7de303c90ae65b142c33787fae039213c7962ae5
pallat <yod.pallat@gmail.com>
2017-03-09 22:18:45 +0700

    package

---
+++ 2days/firstday/pack/hello/print.go
@@ -0,0 +1,7 @@
+package hello
+
+import "fmt"
+
+func Println(s string) {
+	fmt.Println("Hello,", s)
+}
---
+++ 2days/firstday/pack/main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "training/2days/firstday/pack/hello"
+
+func main() {
+	hello.Println("world")
+}
