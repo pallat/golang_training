45cbda8ba92e4e803e860ea894425e722ef705d1
pallat <yod.pallat@gmail.com>
2017-03-13 23:07:39 +0700

    my best

--- 2days/firstday/loop/ex_square.go
+++ 2days/firstday/loop/ex_square.go
@@ -2,39 +2,50 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func Sqrt(x float64) float64 {
-	//var y int64
 	var z float64
 
 	for i := 1; i < 10; i++ {
 		z = float64(i)
-		res := z - ((z*z)-x)/(2*z)
-		if ((res * res) - x) < 0.1 {
-			k := res - 0.5
-			for j := k; j < (res + 0.5); j += 0.1 {
-				z = j
-				res := z - ((z*z)-x)/(2*z)
-				if ((res * res) - x) < 0.01 {
-					k := res - 0.05
-					for l := k; l < (res + 0.05); l += 0.01 {
-						z = l
-						res := z - ((z*z)-x)/(2*z)
-						if ((res * res) - x) < 0.001 {
-							return res
-						}
-					}
-				}
+		res := fomular(z, x)
+		if isclosest(res, 1, x) {
+			for j := 10.00; j < 10000000000; j *= 10 {
+				res = closest(res, 1/j, x)
 			}
-			//			z = float64(i-1)
-			//			return z - ((z*z) - x) / (2 * z)
 			return res
 		}
 	}
-	return z * 1000
+	return x
+}
+
+func closest(near, ratio, x float64) float64 {
+	min := near - (ratio * 5)
+	max := near + (ratio * 5)
+	for i := min; i <= max; i += ratio {
+		res := fomular(i, x)
+		if (res * res) == x {
+			return res
+		}
+		if isclosest(res, ratio, x) {
+			return res
+		}
+	}
+	return 0
+}
+
+func isclosest(res, ratio, x float64) bool {
+	return ((res * res) - x) < (ratio / 3)
+}
+
+func fomular(z, x float64) float64 {
+	return z - (((z * z) - x) / (2 * z))
 }
 
 func main() {
-	fmt.Println(Sqrt(3))
+	var g float64
+	fmt.Fscanln(os.Stdin, &g)
+	fmt.Println(Sqrt(g))
 }
