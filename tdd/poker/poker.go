a0df5170af803acddc197038d36b808b7b744293
pallat <yod.pallat@gmail.com>
2017-04-09 22:45:01 +0700

    black wins by rank

--- tdd/poker/poker.go
+++ tdd/poker/poker.go
@@ -1,5 +1,9 @@
 package poker
 
+import (
+	"strconv"
+)
+
 type card struct {
 	rank string
 	suit string
@@ -8,5 +12,43 @@ type card struct {
 type cards []card
 
 func compare(black, white cards) string {
+	mxBlck, mxWhte := 0, 0
+
+	for _, v := range black {
+		if val(v.rank) > mxBlck {
+			mxBlck = val(v.rank)
+		}
+	}
+	for _, v := range white {
+		if val(v.rank) > mxWhte {
+			mxWhte = val(v.rank)
+		}
+	}
+
+	if mxBlck > mxWhte {
+		return "Black wins - high card: Ace"
+	}
+
 	return "White wins - high card: Ace"
+}
+
+func val(s string) int {
+	mapping := map[string]string{
+		"T": "10",
+		"J": "11",
+		"Q": "12",
+		"K": "13",
+		"A": "14",
+	}
+
+	if v, ok := mapping[s]; ok {
+		s = v
+	}
+
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+
+	return n
 }
--- tdd/poker/poker_test.go
+++ tdd/poker/poker_test.go
@@ -23,3 +23,25 @@ func TestWhiteWinsByScore(t *testing.T) {
 		t.Error("result should be 'White wins - high card: Ace' but was", result)
 	}
 }
+
+func TestBlackWinsByScore(t *testing.T) {
+	white := []card{
+		card{rank: "2", suit: "H"},
+		card{rank: "3", suit: "D"},
+		card{rank: "5", suit: "S"},
+		card{rank: "9", suit: "C"},
+		card{rank: "K", suit: "D"},
+	}
+	black := []card{
+		card{rank: "2", suit: "C"},
+		card{rank: "3", suit: "H"},
+		card{rank: "4", suit: "S"},
+		card{rank: "8", suit: "C"},
+		card{rank: "A", suit: "H"},
+	}
+
+	result := compare(black, white)
+	if result != "Black wins - high card: Ace" {
+		t.Error("result should be 'Black wins - high card: Ace' but was", result)
+	}
+}
